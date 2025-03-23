bfa0a7450e30ba83b849b5ef9440f8033325573b
tumbashka <tumbashka@gmail.com>
2025-03-23 23:38:07 +0400

    Fix cache clear on student deleting

--- app/Listeners/ClearUserAllLessonsCache.php
+++ app/Listeners/ClearUserAllLessonsCache.php
@@ -2,11 +2,6 @@
 
 namespace App\Listeners;
 
-use App\Events\LessonTime\LessonTimeAdded;
-use App\Events\LessonTime\LessonTimeDeleted;
-use App\Events\LessonTime\LessonTimeUpdated;
-use App\Events\Student\StudentDeleted;
-use App\Events\Student\StudentUpdated;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
@@ -29,6 +24,8 @@ class ClearUserAllLessonsCache
         Log::info('Clear User All Lessons Cache');
         if ($model instanceof \App\Models\LessonTime) {
             $user = $model->student->user;
+        } elseif ($model instanceof \App\Models\Student) {
+            $user = $model->user;
         } else {
             return;
         }
